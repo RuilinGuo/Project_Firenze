084ce4390956809f4f709cbb5f8cf5176d869b58
Ruiling Guo <ruiling.guo@thoughtworks.com>
2021-05-18 11:44:01 +0800

    FEAT: add high card comparing

---
+++ src/main/java/domain/poker/rankingCompare/HighCardComparing.java
@@ -0,0 +1,23 @@
+package domain.poker.rankingCompare;
+
+import domain.poker.ranking.RankingResult;
+
+import java.util.Comparator;
+
+public class HighCardComparing implements Comparator<RankingResult> {
+
+    @Override
+    public int compare(RankingResult o1, RankingResult o2) {
+        int size = o1.getCards().size();
+
+        for (int i = 0; i < size; i++) {
+            if (o1.getCards().get(i).getPointNumber() < o2.getCards().get(i).getPointNumber()) {
+                return 1;
+            }
+            if (o1.getCards().get(i).getPointNumber() > o2.getCards().get(i).getPointNumber()) {
+                return -1;
+            }
+        }
+        return 0;
+    }
+}
